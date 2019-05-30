bdbfdd9196f4b129e11c77ba7debd3a3d525282f
fastjames <fastjames@gmail.com>
2019-05-30 15:29:33 -0500

    Fix typo in feed edit form
    Fix a minor typo in the product_feed form view template.

---
+++ spec/requests/manage_feeds_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+RSpec.describe 'feed management', type: :request do
+  stub_authorization!
+
+  it 'allows a custotmer to edit a feed' do
+    get '/admin/product_feeds/new'
+    expect(response).to render_template(:new)
+  end
+end
